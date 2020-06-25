5a8c6fc8c499b9336fad222003fd23ac1a8536f1
aakula@ucla.edu <aakula@ucla.edu>
2020-06-25 11:40:44 -0700

    linux

---
+++ run_train.sh
@@ -0,0 +1,1 @@
+python train.py --train_new --train_dir vcla1007/training_data/train --dev_dir vcla1007/training_data/val --test_dir vcla1007/training_data/test --output_dir vcla1007/models/model1 --batch_size 10  --num_epochs 5000 --fp16
