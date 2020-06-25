2bfc28848733d3875296904d2a069dee42eaa32d
kezewang <kezewang@gmail.com>
2020-06-25 16:12:35 -0700

    train_pickle

--- checkSavedTrainingData.py
+++ checkSavedTrainingData.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-path = 'C:\\Users\\kezew\\Dropbox\\work\\OnGoingProjects\\Self-Driving-Car-in-Video-Games\\vcla1007\\training_data\\training_data3.npz'
+path = 'C:\\Users\\kezew\\Dropbox\\work\\OnGoingProjects\\Self-Driving-Car-in-Video-Games\\vcla1007\\raw_data\\training_data30.npz'
 
 
 data = np.load(path, allow_pickle=True)["arr_0"]
@@ -9,7 +9,7 @@ data = np.load(path, allow_pickle=True)["arr_0"]
 print(len(data))
 dist_data = list()
 count = 0
-for data_i in data[:-3]:
+for data_i in data:#[:-1]:
     for data_i_j in data_i[:-1]:
         # print(data_i_j)
         cv2.imshow("data", data_i_j)
@@ -18,5 +18,5 @@ for data_i in data[:-3]:
     print(count)
     count += 1
 
-file_name = 'C:\\Users\\kezew\\Dropbox\\work\\OnGoingProjects\\Self-Driving-Car-in-Video-Games\\vcla1007\\training_data\\filted_training_data3.npz'
+file_name = 'C:\\Users\\kezew\\Dropbox\\work\\OnGoingProjects\\Self-Driving-Car-in-Video-Games\\vcla1007\\raw_data\\filted_training_data30.npz'
 np.savez(file_name, dist_data)
